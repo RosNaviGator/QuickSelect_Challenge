e22d81b7ad97a2c1064feae3a15b930b95570668
MarcoCioci <marco.cioci@mail.polimi.it>
2024-04-24 16:15:53 +0200

    .at() into [ ]

--- src/challenge.cpp
+++ src/challenge.cpp
@@ -4,7 +4,7 @@
 #include <random>
 #include <chrono>
 
-#define seed 24024224524
+#define seed 24042024
 #define N 100
 #define MAX 1000
 
@@ -12,37 +12,39 @@ using namespace std;
 using vectInt = vector<int>;
 using llu = long long unsigned int;
 
-void newSwap(vectInt& a, int i, int j)
+
+void newSwap(vectInt& vect, int i, int j)
 {
-    int t = a[i];
-    a[i] = a[j];
-    a[j] = t;
+    int temp = vect[i];
+    vect[i] = vect[j];
+    vect[j] = temp;
 }
 
 void printVec(vectInt& vect)
 {
     for(llu i=0; i< vect.size(); i++)
     {
-        cout << vect.at(i) <<" ";
+        cout << vect[i] <<" ";
     }
     cout << endl << "size is " << vect.size() <<endl;
+    return;
 }
 
-int sort5(vector<int> vect, vector<int>& index, int i) 
+int sort5(vectInt vect, vectInt& index, int i) 
 {
 
     for( int j= i*5; j< 5*(i+1)-1; j++)
     {
         for(int z = i*5; z < 5*(i+1)-1; z++)
         {
-            if (vect.at(z) > vect.at(z+1))
+            if (vect[z] > vect[z+1])
             {
-                newSwap (vect, z, z+1);
-                newSwap (index, z, z+1);
+                newSwap(vect, z, z+1);
+                newSwap(index, z, z+1);
             }
         }
     }
-    return (index.at(5*i+2));
+    return (index[5*i+2]);
 
 }
 
@@ -122,11 +124,12 @@ int newSort(vectInt& vect, vectInt& index, int i)
 }
 
 
-int sortGroupsOf5(vectInt& vect, vectInt& index)
+
+int median(vectInt& vect, vectInt& index)
 {
    if (index.size() < 5)
    {
-       return index.at(index.size()/2);
+       return index[index.size()];
    }
    int end = index.size();
    int blocks = index.size()/5;
@@ -135,53 +138,39 @@ int sortGroupsOf5(vectInt& vect, vectInt& index)
    {
     vect2.push_back(newSort(vect, index, i));
    } 
-   return sortGroupsOf5(vect, vect2);
+   return median(vect, vect2);
     
-}
-
-
-
-int partition(vectInt& vect, int begin, int end)
-{
-    
-    vectInt indexes;
-
-    for(int i=0; i<vect.size(); i++)
-        indexes.push_back(i);
-
-    return sortGroupsOf5(vect, indexes);
-
 }
 
 
 int pivPartition(int index, vectInt& vect, int start, int end)
 {   
     
-    swap (vect.at(index), vect.at(end));
-    int newPivot = vect.at(end);
+    newSwap (vect, index, end);
+    int newPivot = vect[end];
     int i = start - 1;
 
-    for (int j = start; j < end; ++j) // Changed end - 1 to end
+    for (int j = start; j < end; ++j) 
     {
-        if (vect.at(j) <= newPivot)
+        if (vect[j] <= newPivot)
         {
             i++;
-            swap (vect.at(i), vect.at(j));
+            newSwap (vect, i, j);
         }
     }
 
     // Swap the pivot to its correct position
-    swap (vect.at(i+1), vect.at(end));
+    newSwap (vect, i+1, end);
     return i + 1;
 }
 
 
-int select(vectInt& vect, vectInt& index, int p, int q, int i)
+int quickSelect(vectInt& vect, vectInt& index, int p, int q, int i)
 {
     if (p >= q)
         return index[p];
 
-    int pivot = sortGroupsOf5(vect, index);
+    int pivot = median(vect, index);
     int r = pivPartition (pivot, vect, p, q);
     int k = r - p + 1;
 
@@ -189,25 +178,24 @@ int select(vectInt& vect, vectInt& index, int p, int q, int i)
         return index[r];
     else if (i < k)
         {   
-            return select(vect, index, p, r - 1, i);
+            return quickSelect(vect, index, p, r - 1, i);
         } 
     else
         {
-            return select(vect, index, r + 1, q, i - k);
+            return quickSelect(vect, index, r + 1, q, i - k);
         }      
 }
 
 int main()
 {
     vectInt vect, index;
-
     for(int i=0; i<N; i++)
     {
         vect.push_back(i+1);
         index.push_back(i);
     }
 
-    int k = select(vect, index, 0, N-1, 25 );
-    cout <<endl <<"k: " <<k <<endl;
+    int k = quickSelect(vect, index, 0, N-1, 25 );
+    cout <<"k: " <<k <<endl;
 
 }
